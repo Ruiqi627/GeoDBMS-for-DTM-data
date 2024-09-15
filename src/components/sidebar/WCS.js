b89f26cd91f2115a5df4239dea0e598752af871f
Ruiqi627 <109479078+Ruiqi627@users.noreply.github.com>
2024-09-15 23:32:07 +0200

    Update WCS.js

--- src/components/sidebar/WCS.js
+++ src/components/sidebar/WCS.js
@@ -1,1 +1,50 @@
+import React from 'react';
+
+const WCS = ({
+    wcsalsname,
+    setWcsalsname,
+    wcsfilename,
+    setWcsfilename,
+    FilenameOptions,
+    ALSnameOptions,
+    setWcsdraw,
+    setClearWCS,
+    setCleargeoraster
+}) => {
+    const WCSdrawHandler = () => {
+        setWcsdraw(true);
+    };
+
+    return (
+        <div>
+            <h3>Load by WCS</h3>
+            <p>Please choose the ALS name:</p>
+            <select value={wcsalsname} onChange={(e) => setWcsalsname(e.target.value)}>
+                {ALSnameOptions.map(option => (
+                    <option key={option} value={option}>
+                        {option}
+                    </option>
+                ))}
+            </select>
+            <p>Please choose the file name:</p>
+            <select value={wcsfilename} onChange={(e) => setWcsfilename(e.target.value)}>
+                {FilenameOptions.map(option => (
+                    <option key={option} value={option}>
+                        {option}
+                    </option>
+                ))}
+            </select>
+            <p>Please draw the envelope</p>
+            <button onClick={WCSdrawHandler}>Draw</button>
+            <button onClick={() => {
+                setClearWCS(true);
+                setCleargeoraster(true);
+            }}>
+                Clear
+            </button>
+        </div>
+    );
+};
+
+export default WCS;
 
